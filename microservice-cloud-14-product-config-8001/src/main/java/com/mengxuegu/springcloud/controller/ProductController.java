83cb153e994667f0581e978699eecfa8db6473a6
xianchen <super_cx@163.com>
2020-08-04 16:57:09 +0800

    docker

--- microservice-cloud-14-product-config-8001/src/main/java/com/mengxuegu/springcloud/controller/ProductController.java
+++ microservice-cloud-14-product-config-8001/src/main/java/com/mengxuegu/springcloud/controller/ProductController.java
@@ -3,12 +3,23 @@ package com.mengxuegu.springcloud.controller;
 import com.mengxuegu.springcloud.entities.Product;
 import com.mengxuegu.springcloud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RefreshScope //刷新配置
 @RestController
 public class ProductController {
+
+    @Value("${emp.name}")
+    private String name;
+    @GetMapping("/hello")
+    public String hello() {
+        return name;
+    }
+
     @Autowired
     private ProductService productService;
 
